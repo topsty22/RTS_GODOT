9cb7471c17097d34c2ddcfb5fd6eb2565cb7e5eb
topsty22 <germain.giai@gmail.com>
2024-07-21 19:51:30 +0200

    Gamescene script change and camera change

--- scripts/GameScene.gd
+++ scripts/GameScene.gd
@@ -1,35 +1,40 @@
-extends Node2D
+extends Node3D
 
-@export var tile_size = Vector2(64, 32)
-@export var camera_speed = 2.0  # Camera movement speed
+@export var tile_size = Vector3(64, 32, 16)
+@export var camera_speed = 2.5  # Camera speed
 
 # Variables
-var camera: Camera2D
-var tilemap: TileMap
-var drag_start_position: Vector2
+var camera: Camera3D
+var drag_start_position: Vector2  # Vector2 for screen coordonates
+var is_dragging: bool = false
 
 func _ready():
-	
-	camera = $Camera2D
-	camera.make_current()  # Set the camera
-	camera.zoom = Vector2(1, 1)  # Zoom
-
-	tilemap = $TileMap
+	camera = $Camera/Camera3D
+	camera.make_current()  # Set camera
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
-				# Store the mouse position when left button is pressed
-				drag_start_position = get_viewport().get_mouse_position()
-			elif event.button_mask == 0:  # Check if no buttons are pressed (button released)
-				drag_start_position = Vector2.ZERO  # Reset drag start position
+				# Store mouse position when using left button
+				drag_start_position = event.position
+				is_dragging = true
+			else:
+				# Reset drag start position when click is released
+				is_dragging = false
 
 func _process(delta):
-	# Check if the left mouse button is being held down
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		# Calculate the movement vector based on the difference between current and start drag positions
-		var drag_delta = get_viewport().get_mouse_position() - drag_start_position
+	if is_dragging:
+		# Calculate vector movement based on difference between current and start drag position
+		var current_position = get_viewport().get_mouse_position()
+		var drag_delta = current_position - drag_start_position
 
-		# Adjust the camera position
-		camera.offset -= drag_delta * delta * camera_speed
+		# Convert drag delta to world coordinates
+		var move_x = -drag_delta.x * delta * camera_speed
+		var move_y = -drag_delta.y * delta * camera_speed
+
+		# Move the camera in x and y direction
+		camera.translate(Vector3(move_x, move_y, 0))
+		
+		# Update drag start position
+		drag_start_position = current_position
